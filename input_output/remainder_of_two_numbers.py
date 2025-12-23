c091f87f17ca42a003ae751479173d9993e36db2
Kilak Pandey <kilakpandey@gmail.com>
2025-12-23 14:27:18 +0530

    Update remainder_of_two_numbers.py

--- input_output/remainder_of_two_numbers.py
+++ input_output/remainder_of_two_numbers.py
@@ -2,4 +2,4 @@ a = 421
 
 b = 33
 
-print("Remainder of a when devided by b is:", a % b ) # & the symbol here represents modulo operator which let's you find remainder
+print("Remainder of a when devided by b is:", a % b ) # % the symbol here represents modulo operator which let's you find remainder
